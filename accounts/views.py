13bcddee694c34ab00b9ad10ca35960f0d798b22
Spencer Hargis <shargis664@gmail.com>
2026-01-26 12:26:55 -0500

    Created a login/logout and a superuser for testing.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("dashboard-home")
+
+    form = AuthenticationForm()
+
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("dashboard-home")
+
+    #return render(request, "accounts/debug.html", {})
+    return render(request, "accounts/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
--- cti/settings.py
+++ cti/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'dashboard',
+    'accounts',
+    'indicators',
+    'api',
+    'ingestion',
+    'processors',
+    'scripts',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
